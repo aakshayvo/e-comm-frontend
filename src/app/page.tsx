bd9576b71d9eca687e5c1d7a7e9826ae3c7120bb
akshay v omkar <akshay.v.omkar@codeandtheory.com>
2024-12-24 10:06:26 +0530

    minor changesBrands Component.component";

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import BrandsComponent from "@/components/brands/BrandsComponent"; //becuase I have changed the name of the folder getting issue while deploying
+import BrandsComponent from "@/components/brands/BrandsComponent";
 import HeroComponent from "@/components/hero-section/HeroComponent";
 import NewProduct from "@/components/new-product/NewProduct";
 import Testimonial from "@/components/testimonial/Testimonial";
