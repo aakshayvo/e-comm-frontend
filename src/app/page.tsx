74518aafe12610e1ed481aaf8d00bd75775c2a85
akshay v omkar <akshay.v.omkar@codeandtheory.com>
2024-12-28 21:01:32 +0530

    clear cache and re-add files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import BrandsComponent from "@/components/brands/BrandsComponent";
+
 import HeroComponent from "@/components/hero-section/HeroComponent";
 import NewProduct from "@/components/new-product/NewProduct";
 import Testimonial from "@/components/testimonial/Testimonial";
+import BrandsComponent from "@/components/brands/BrandsComponent";
 
 export default function Home() {
   return (
